edb9163d85b64b6300fa608fb04f3a148136b983
selinaljx <selinalaishe1118@gmail.com>
2016-02-23 15:09:10 +0800

    getcwd and input argv

--- assignments/phase1/simple_shell.c
+++ assignments/phase1/simple_shell.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
+#include <limits.h> //PATH_MAX
+#include <unistd.h> //getcwd()
+
 
 int main (int argc, char *argv[]){
+	char buf[255];
+	char cwd[PATH_MAX+1];
+	
+	// get current directory
+	if (getcwd(cwd, PATH_MAX+1) == NULL){
+		printf("Error occurred\n");
+		return 0;
+	}
+	
+	// get user input command
+	printf("[3150 shell: %s]$ ", cwd);
+	fgets(buf, 255, stdin);
+	printf("%s\n", buf);
 
-	printf("[3150 shell:]\n");
 	return 0;
 }
